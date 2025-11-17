8bac09612233a68774d74868c08f75587959f0cf
kizzy000 <faiz24ti@mahasiswa.pcr.ac.id>
2025-11-17 15:25:25 +0700

    p11

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateFirstUser extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'gatot@pcr.ac.id',
+            'password' => Hash::make('gatotkaca')
+        ]);
+    }
+}
