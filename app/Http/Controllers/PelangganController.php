c03598107f71d9e1cc42434c6482d641cdaaf63a
kizzy000 <faiz24ti@mahasiswa.pcr.ac.id>
2025-11-24 15:25:40 +0700

    P11

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -10,10 +10,19 @@ class PelangganController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data['dataPelanggan'] = Pelanggan::all();
+        $filterableColumns = ['gender'];
+        $searchableColumns = ['first_name'];
+        $data['dataPelanggan'] = Pelanggan::filter($request, $filterableColumns)
+                                                ->search($request, $searchableColumns)
+                                                ->paginate(10)
+                                                ->onEachSide(2)
+                                                ->withQueryString();
         return view('admin.pelanggan.index', $data);
+
+        // $data['dataPelanggan'] = Pelanggan::simplePaginate(10);
+        // return view('admin.pelanggan.index', $data);
     }
 
     /**
